3128f4925b160b7278e89cf976a045219aa9ba40
Christian-Cell <cristohp74@gmail.com>
2020-03-30 21:44:32 +0200

    sanp de la practica de funciones java

---
+++ practica.js
@@ -0,0 +1,27 @@
+//en este ejemplo mi objetivo es reproducir el arrayImpares en el lugar que yo quiera
+//usando la funcion numerosImpares escribiendo "numerosImpares("impar")" en cualquier
+//lugar del documento.
+
+function numerosImpares(impar){
+    console.log("listado impar:");
+    var arrayImpares = [35,43,41,31,7,91,93,73,69,53,73,49,15,91,17,59,89,39,67,89,77,29,67,59,93];
+   console.log("listado impar"+[35,43,41,31,7,91,93,73,69,53,73,49,15,91,17,59,89,39,67,89,77,29,67,59,93]);
+}
+
+numerosImpares("impar");
+
+//en este ejemplo mi objetivo es reproducir el arrayPares en el lugar que yo quiera
+//usando la funcion numerosPares escribiendo "numerosPares("par")" en cualquier
+//lugar del documento.
+
+
+function numerosPares(par){
+    console.log("listado par:");
+    var arrayPares = [58,52,84,16,42,38,78,94,30,28,74,64,26,32,64,60,10,32,38,40,8,40,62,22];
+   console.log("listado par"+[58,52,84,16,42,38,78,94,30,28,74,64,26,32,64,60,10,32,38,40,8,40,62,22]);
+}
+
+numerosPares("par");
+numerosPares("par");
+numerosImpares("impar");
+numerosImpares("impar");
